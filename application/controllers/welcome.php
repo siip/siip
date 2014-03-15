1630743219ecd622e9ff1e828aa3da4d94b8e8b2
paxmus <paxmus@gmail.com>
2014-03-15 11:07:08 +0100

    test git commit

--- application/controllers/welcome.php
+++ application/controllers/welcome.php
@@ -13,21 +13,6 @@ class Welcome extends CI_Controller {
 		$this -> em = $this -> doctrine -> em;
 	}
 
-	/**
-	 * Index Page for this controller.
-	 *
-	 * Maps to the following URL
-	 * 		http://example.com/index.php/welcome
-	 *	- or -
-	 * 		http://example.com/index.php/welcome/index
-	 *	- or -
-	 * Since this controller is set as the default controller in
-	 * config/routes.php, it's displayed at http://example.com/
-	 *
-	 * So any other public methods not prefixed with an underscore will
-	 * map to /index.php/welcome/<method_name>
-	 * @see http://codeigniter.com/user_guide/general/urls.html
-	 */
 	public function index() {
 		//$this->load->view('welcome_message');
 		//$userRepository = $this -> em -> getRepository('Entity/User');
